d5fd81a01b367e9b65ee6c8734805740f0a0cc4d
Mouad Lahlal <lahlalmouad@gmail.com>
2025-01-17 10:57:52 +0100

    Cleaned and optimezed code

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,53 @@
 use std::fs;
 
 fn main() {
-    let contents = fs::read_to_string("/path/to/file")
+    let contents = fs::read_to_string("/home/mouad/Documents/notes/personal/lifeprogress.md")
         .expect("Should have been able to read the file");
     let contents: Vec<&str> = contents.split('\n').collect();
     let mut lines = vec![];
-    let mut day = String::new();
+    let mut day = "";
     let mut tasks: bool = false;
     let mut points: i32 = 0;
     let lvl;
 
     for line in &contents {
-        let tmp: String = line.to_string();
+        let tmp = line;
 
-        if tasks && tmp.chars().count() >= 2 && &tmp[..3] == "- [" {
+        if tasks && tmp.len() >= 2 && &tmp[..3] == "- [" {
             let tmp_pt = points;
-            match &tmp[6..7] {
-                "H" => {
-                    points = points + ( if &tmp[3..4] == "X" {10} else {-15} );
-                },
-                "M" => {
-                    points = points + ( if &tmp[3..4] == "X" {5} else {-3} );
-                },
-                "L" => {
-                    points = points + ( if &tmp[3..4] == "X" {1} else {0} );
-                },
-                _ => println!("Stop The Cap"),
-            }
-            if tmp_pt < points { String::clear(&mut day); }
+            let done = &tmp[3..4];
+            let priority = &tmp[6..7];
+
+            points += match (done, priority) {
+                ("X", "H") => 10,
+                (_, "H") => -15,
+                ("X", "M") => 5,
+                (_, "M") => -3,
+                ("X", "L") => 1,
+                (_, "L") => 0,
+                _ => {
+                    println!("Invalid priority");
+                    0
+                }
+            };
+
+            if tmp_pt < points { day = ""; }
+
             continue;
-        } else {
-            if day.len() > 0 && tasks { points = points - 10; }
+        } else if day.len() > 0 && tasks {
+            points -= 10;
             tasks = false;
         }
 
-        if tmp.chars().count() >= 6 && &tmp[..6] == "Punti:" {
+        if tmp.len() >= 6 && &tmp[..6] == "Punti:" {
             let tmp_pt = tmp.chars().skip_while(|c| !c.is_digit(10) && *c != '-').collect::<String>();
             let tmp_pt = tmp_pt.parse::<i32>().unwrap();
             points = points + tmp_pt;
         }
 
-        if tmp.chars().count() >= 4 && &tmp[0..4] == "####" {
+        if tmp.len() >= 4 && &tmp[0..4] == "####" {
             tasks = true;
-            day = tmp.clone();
+            day = tmp;
         }
 
         lines.push(format!("..{}..", tmp));
