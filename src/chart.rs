c53cd088bf26ba7faa8b852ebfe6aecb1a8d194f
mlahlal <lahlalmouad@gmail.com>
2025-03-17 01:42:09 +0100

    Fix and refactor

--- src/chart.rs
+++ src/chart.rs
@@ -4,13 +4,12 @@ use std::collections::HashMap;
 use plotters::prelude::*;
 
 pub fn draw_chart(data: &HashMap<String, i32>) -> Result<(), Errcode> {
-    let root = BitMapBackend::new("chart.png", (1366, 720)).into_drawing_area();
+    let root = BitMapBackend::new("chart.png", (1920, 720)).into_drawing_area();
     let _ = root.fill(&WHITE);
+    let mut data_points: Vec<(&str, i32)> = data.iter().map(|(date, value)| (date.as_str(), *value)).collect();
 
-    let tmp: Vec<String> = data.keys().cloned().collect();
-    let mut x_labels: Vec<&str> = tmp.iter().map(AsRef::as_ref).collect();
-    x_labels.sort_by(|a, b| a.cmp(b));
-    x_labels.sort_by(|a, b| a.split("/").last().unwrap().cmp(b.split("/").last().unwrap()));
+    data_points.sort_by(|a, b| a.0.cmp(b.0));
+    data_points.sort_by(|a, b| a.0.split("/").last().unwrap().cmp(b.0.split("/").last().unwrap()));
 
     let mut chart = match ChartBuilder::on(&root)
         .caption("Total Tasks per Day", ("sans-serif", 50).into_font())
@@ -18,7 +17,7 @@ pub fn draw_chart(data: &HashMap<String, i32>) -> Result<(), Errcode> {
         .y_label_area_size(10)
         .margin(10)
         .build_cartesian_2d(
-            x_labels.as_slice(),
+            0..data_points.len()-1,
             0..*data.values().max().unwrap_or(&0)+2,
         ) {
             Ok(chart) => chart,
@@ -30,20 +29,25 @@ pub fn draw_chart(data: &HashMap<String, i32>) -> Result<(), Errcode> {
 
     if let Err(e) = chart
         .configure_mesh()
-        .x_labels(30)
+        .x_labels(data_points.len()/2)
         .y_labels(30)
-        .x_label_formatter(&|x| x.to_string())
+        .disable_x_mesh()
+        .x_label_formatter(&|x| data_points[*x].0.to_string())
         .y_label_formatter(&|y| y.to_string())
         .draw() {
             log::error!("Error configuring mesh");
             return Err(Errcode::UnknownError(e.to_string()));
     }
 
-    let data_points: Vec<(&str, i32)> = data.iter().map(|(date, value)| (date.as_str(), *value)).collect();
+    if let Err(e) = chart.draw_series(
+        data_points.iter().zip(0..data_points.len()).map(|tuple| Circle::new((tuple.1, tuple.0.1), 2, RED.filled())),
+    ) {
+        log::error!("Error drawing series");
+        return Err(Errcode::UnknownError(e.to_string()));
+    }
 
     if let Err(e) = chart.draw_series(
-        data_points.iter().map(|(date, value)| Circle::new((date, *value), 5, RED.filled())),
-        //LineSeries::new(data_points.iter().map(|(date, value)| (date, *value)), RED)
+        LineSeries::new(data_points.iter().zip(0..data_points.len()).map(|tuple| (tuple.1, tuple.0.1)), RED),
     ) {
         log::error!("Error drawing series");
         return Err(Errcode::UnknownError(e.to_string()));
